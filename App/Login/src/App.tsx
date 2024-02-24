fde596dd0ac4be07e5062544772ea6a1d62287fd
Fuzail Khan <fuzailxkhan@gmail.com>
2024-02-24 20:54:51 +0500

    Added JWT Token

--- App/Login/src/App.tsx
+++ App/Login/src/App.tsx
@@ -5,6 +5,7 @@ import { FieldValues } from "react-hook-form"
 import axios from "axios"
 import SignUpModal from "./components/SignUpModal"
 import LoginModal from "./components/LoginModal"
+import { jwtDecode } from "jwt-decode"
 
 const App = () => {
 
@@ -13,6 +14,7 @@ const App = () => {
     const [serverResponse,setServerResponse] = useState("");
     const [loginData,setLoginData] = useState<FieldValues>();
     const [sendLogin,setSendLogin] = useState(false);
+    const [role,setRole] = useState("");
 
     useEffect(()=>{
       const controller = new AbortController();
@@ -26,6 +28,7 @@ const App = () => {
       }
 
       
+      
       return ()=>controller.abort();
     },[sendCreateAcc])
 
@@ -34,7 +37,7 @@ const App = () => {
 
       if(sendLogin){
         axios.post("http://localhost:3000/loginAccount",loginData,{signal:controller.signal})
-        .then(res=>{console.log(res);setServerResponse(res.data.Message);})
+        .then(res=>{console.log(res);setServerResponse(res.data.Message);const token = jwtDecode<{uid:string,role:string}>(res.data.token) ;setRole(token.role)})
         .catch(err=>console.log(err))
         .finally(()=>{setSendLogin(false);})
         console.log(loginData)
@@ -44,14 +47,20 @@ const App = () => {
       return ()=>controller.abort();
     },[loginData])
 
+    const handleButton = () =>{
+      axios.post("http://localhost:3000/addProduct")
+      
+    }
+
     
   return (
     <>
-      <Navbar/>
+      <Navbar role={role}/>
       <div className="background-div">
         <div className="main-div">
           <SignUpModal onCreateAccount={(data)=>{setCreateAccData(data);setSendCreateAcc(true);}} serverResponse={serverResponse}/>
           <LoginModal onLogin={(data)=>{setLoginData(data);setSendLogin(true);}} serverResponse={serverResponse}/> 
+          <button onClick={handleButton} />
         </div>
       </div>
     </>
--- Server/server/server.js
+++ Server/server/server.js
@@ -4,14 +4,17 @@ const fs = require('fs');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const userSchema = require('./schema/schema');
 
 const secret_key = "RimshaAnwar4ever"
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
-const userSchema = require('./schema/schema')
+
 
 mongoose.connect('mongodb+srv://fuzail:spiderman123@cluster0.1ig1y1c.mongodb.net/?retryWrites=true&w=majority')
 .then(()=>console.log("Connected To Database"))
@@ -27,19 +30,16 @@ const emailFind=async(email)=>{
     return user;
 }
 
-
-
-
 app.post("/createAccount",async (req,res)=>{
     console.log(req.body)
     const user = await emailFind(req.body.email);
     if(user.length <=0){
         const salt = await bcrypt.genSalt(10);
         const pass = await bcrypt.hash(req.body.password,salt);
-        const newUser = userSchema({email:req.body.email,password:pass});
+        const newUser = userSchema({email:req.body.email,password:pass,role:"User"});
         const savedUser = await newUser.save()
         console.log(savedUser);
-        const token = jwt.sign({id:savedUser.id,email:savedUser.email},secret_key)
+        const token = jwt.sign({id:savedUser.id,email:savedUser.email,role:savedUser.role},secret_key)
 
         res.status(200).json({"Message":"New Account Created on "+newUser.email,"token":token});
         
@@ -51,20 +51,23 @@ app.post("/createAccount",async (req,res)=>{
 })
 
 
-
-
 app.post("/loginAccount",async (req,res)=>{
     console.log(req.body);
     const foundUser = await emailFind(req.body.email);
-    if(bcrypt.compare(req.body.password,foundUser[0].password)){        
-    res.status(200).json({"Message":"You are Logged in"})
+    if(bcrypt.compare(req.body.password,foundUser[0].password)){
+        var token = jwt.sign({uid:foundUser[0].id,role:foundUser[0].role}, secret_key, {expiresIn:'1h'});
+        res.status(200).cookie("x-jwt-token",token ,{expires:new Date(Date.now()+ 3*24*60*60*1000),httpOnly:true}).json({"Message":"You are Logged in"})
     }
     else{
         res.status(200).json({"Message":"Incorrect Password"})
     }
 })
 
-
+app.post("/addProduct",async (req,res)=>{
+    console.log(req.cookies);
+    console.log(req.signedCookies);
+    res.json("Data Recieved");
+})
 
 
 app.listen(3000,(err)=>{
